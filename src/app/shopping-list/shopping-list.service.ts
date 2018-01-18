16e1652db050a17b74527b0a94c97d4c8b9413e6
Brahmaiah Gunji <nanibrahmaiag@gmail.com>
2018-01-18 17:59:47 +0530

    Update

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -5,15 +5,20 @@ import { Subject } from 'rxjs/Subject';
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
 
     private ingredients:Ingredient[] = [
         new Ingredient('Apple',5),
-        new Ingredient('Tomato',10)
+        new Ingredient('Bread',10)
       ];
 
      getIngredients(){
          return this.ingredients.slice();
-     } 
+     }
+     
+     getIngredient(index:number){
+        return this.ingredients[index];
+     }
 
      addIngredient(ingredient:Ingredient){
         this.ingredients.push(ingredient);
@@ -24,5 +29,10 @@ export class ShoppingListService {
      addIngredients(ingredients:Ingredient[]){
         this.ingredients.push(...ingredients);
         this.ingredientsChanged.next(this.ingredients.slice());
+     }
+
+     updateIngredient(index:number,newIngredient:Ingredient){
+         this.ingredients[index] = newIngredient;
+         this.ingredientsChanged.next(this.ingredients.slice());
      }
 }
