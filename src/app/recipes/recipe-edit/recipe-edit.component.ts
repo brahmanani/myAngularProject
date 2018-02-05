9df89e767cdb4979382265eeb4a434a6f29e8b26
brahmanani <nanibrahmaiah@gmail.com>
2018-02-05 08:11:07 +0530

    5-FEB-2018 Updated.

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute,Params } from '@angular/router';
+import { Recipe } from '../recipe.model';
+
+import { ActivatedRoute,Params,Router } from '@angular/router';
 import { FormGroup,FormControl,FormArray,Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 
@@ -14,7 +16,7 @@ export class RecipeEditComponent implements OnInit {
   editChange=false;
   recipeForm:FormGroup;
 
-  constructor(private route:ActivatedRoute,private recipeService:RecipeService) { }
+  constructor(private route:ActivatedRoute,private recipeService:RecipeService,private router:Router) { }
 
   ngOnInit() {
     this.route.params
@@ -28,7 +30,22 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log(this.recipeForm);
+    // const newRecipe = new Recipe(
+    //   this.recipeForm.value['name'],
+    //   this.recipeForm.value['description'],
+    //   this.recipeForm.value['imagePath'],
+    //   this.recipeForm.value['ingredients']
+    // )
+    if(this.editChange){
+      this.recipeService.updateRecipe(this.id,this.recipeForm.value);
+    }else{
+      this.recipeService.addRecipe(this.recipeForm.value);
+    }
+    this.onCancel();
+  }
+
+  onCancel(){
+    this.router.navigate(['../'],{relativeTo:this.route});
   }
 
   onAddIngredient(){
@@ -41,6 +58,10 @@ export class RecipeEditComponent implements OnInit {
         ])
       })
     )
+  };
+
+  onDeleteIngredient(index:number){
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
   }
 
   private initForm(){
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 
 import {Recipe} from '../recipes/recipe.model'
 import { Ingredient } from '../shared/Ingredient.model';
@@ -8,6 +9,8 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 @Injectable()
 export class RecipeService {
     //recipeSelected = new EventEmitter<Recipe>();
+    recipesChanged = new Subject<Recipe[]>();
+
     private recipes:Recipe[] = [
         new Recipe(
             'A Test Recipe',
@@ -39,5 +42,20 @@ export class RecipeService {
 
     addIngredientsToShoppingList(ingredients:Ingredient[]){
        this.slService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe:Recipe){
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index:number,newRecipe:Recipe){
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index:number){
+        this.recipes.splice(index,1);
+        this.recipesChanged.next(this.recipes.slice());
     }
 }
